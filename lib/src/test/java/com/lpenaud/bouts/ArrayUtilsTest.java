6380da1692b2ee5d9a08f4b89b7d2f683a94bacb
lpenaud <l.penaud@zaclys.net>
2024-02-03 19:26:37 +0100

    FEAT(Java): Add first snippets

---
+++ lib/src/test/java/com/lpenaud/bouts/ArrayUtilsTest.java
@@ -0,0 +1,53 @@
+package com.lpenaud.bouts;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ArrayUtilsTest {
+
+	private static final String[] ARRAY = { "abc", "bcd" };
+
+	@Test
+	void testCopy() {
+		final var actual = new String[ARRAY.length];
+		ArrayUtils.copy(ARRAY, actual);
+		Assertions.assertArrayEquals(ARRAY, actual);
+	}
+
+	@Test
+	void testCopySrcPosGt() {
+		final Object[] expected = { null, null };
+		final var actual = new String[ARRAY.length];
+		ArrayUtils.copy(ARRAY, actual, 2);
+		Assertions.assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	void testCopyDestLtSrc() {
+		final Object[] expected = { ARRAY[0] };
+		final var actual = new String[expected.length];
+		ArrayUtils.copy(ARRAY, actual);
+		Assertions.assertArrayEquals(expected, actual);
+
+		expected[0] = ARRAY[1];
+		ArrayUtils.copy(ARRAY, actual, 1);
+		Assertions.assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	void testFillObject() {
+		final var singleton = new Object();
+		final Object[] expected = { singleton, singleton, singleton };
+		final var actual = new Object[expected.length];
+		Assertions.assertEquals(actual, ArrayUtils.fill(actual, () -> singleton));
+		Assertions.assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	void testFillInt() {
+		final Object[] expected = { 0, 1, 4 };
+		final var actual = new Object[expected.length];
+		Assertions.assertEquals(actual, ArrayUtils.fill(actual, i -> i * i));
+		Assertions.assertArrayEquals(expected, actual);
+	}
+}
